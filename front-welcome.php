b454b12276e5009bfe3bf6307b3268e4d7752c92
djzach <epicdjzach@gmail.com>
2019-05-16 08:54:48 -0400

    Create front-welcome.php

---
+++ front-welcome.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Your Title Goes Here</title>
+    <!-- Put your CSS in the <style> tags -->
+    <style>
+    </style>
+  </head>
+  
+  <body>
+  
+
+  
+  </body>
+  
+  <?php
+
+  <?
+</html>
