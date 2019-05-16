dd17616b3ee5fcb7e35f63a004e6a189b0343d59
djzach <epicdjzach@gmail.com>
2019-05-16 12:22:21 -0400

    Create bootstrapcss-include-page.php

---
+++ bootstrapcss-include-page.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<meta charset="utf-8">
+
+<?php
+
+  /* 
+  Only includes the bootstrap CSS elements and maps. 
+  */ 
+  
+  include "bootstrap-grid.css";
+  include "bootstrap-grid.css.map";
+  include "bootstrap-grid.min.css";
+  include "bootstrap-grid.min.css.map";
+  include "bootstrap-reboot.css";
+  include "bootstrap-reboot.css.map";
+  include "bootstrap-reboot.min.css";
+  include "bootstrap-reboot.min.css.map";
+  include "bootstrap.css";
+  include "bootstrap.css.map";
+  include "bootstrap.min.css";
+  include "bootstrap.min.css.map";
+
+?>
