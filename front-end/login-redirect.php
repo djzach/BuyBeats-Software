b3eed70c1b2e814a3cae2e4b5bc683cb67852436
djzach <epicdjzach@gmail.com>
2019-05-16 13:52:03 -0400

    Update login-redirect.php

--- front-end/login-redirect.php
+++ front-end/login-redirect.php
@@ -1,1 +1,27 @@
+<?php
+  
+  include "bootstrapcss-include-page.php";
+ 
+  /* 
+  Make this redirect from the "login-page.php"
+  main-page.php -> login-page.php -> login-redirect.php (if the value returns true)
+  Make the php pre-process the form submition, then make the form data go through a validation to return either a true/false value or a PID (don't know yet).
+  If the value is positive, then redirect this to main page with the login value as positive.
+  */
+?>
 
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  
+<head>
+  <meta charset=utf-8>
+  <title>Login Redirect</title>
+</head>
+  
+  
+  
+  <script>
+  
+  </script>
+  
+</html>
