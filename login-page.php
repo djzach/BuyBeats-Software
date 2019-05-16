2f6a93d17351bd8c8613724299777678fa946f23
djzach <epicdjzach@gmail.com>
2019-05-16 08:57:25 -0400

    Create login-page.php

---
+++ login-page.php
@@ -0,0 +1,17 @@
+  <!DOCTYPE html>
+  <html charset="utf-8" dir="ltr">
+  <head>
+  
+    <meta>
+    <title>Login/Sign Up</title>
+    
+  </head>
+  
+  <body>
+    <div>
+      <form id="login-request-form" class="login" name="login-form" method="POST"></form>
+    </div>
+  
+  </body>
+  
+  </html>
