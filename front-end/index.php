7aa241b8788ab0493357238fd26e284806d76a8c
djzach <epicdjzach@gmail.com>
2019-05-17 08:14:36 -0400

    Create index.php

---
+++ front-end/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<?php 
+  include bootstrapcss-include-page.php
+?>
+  
+<html>
+
+  <head>
+    <title>index</title>
+    <link rel = "stylesheet" type = "tect/css" href= "bootstrap-grd.css">
+    <link rel = "stylesheet" type = "tect/css" href= "bootstrap-grd.css.map">
+  </head>
+  <body>
+  
+  
+  </body>
+  
+
+
+</html>
